5a1ecea931f0a6b1a91e105b0fd2392365e00bce
EshginCeferli <eshginij@code.edu.az>
2022-10-02 22:29:58 +0400

    Aspi.Net-intro

---
+++ Aspi.Net-intro/Aspi.Net-intro/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aspi.Net_intro.Controllers
+{
+    public class AboutController:Controller
+    {
+        //public string Index()
+        //{
+        //    return "About page";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aspi.Net-intro/Aspi.Net-intro/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aspi.Net_intro.Controllers
+{
+    public class HomeController:Controller
+    {
+        //public string Index(int id,string name)
+        //{
+        //    return "Home page" + " " + id + "" + name;
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string FullName(string name,string surname)
+        //{
+        //    return "Name" + name + "Surname" + surname;
+        //}
+    }
+}
