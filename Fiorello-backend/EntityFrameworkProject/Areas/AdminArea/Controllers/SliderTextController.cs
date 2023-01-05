49fccb606331e5ee9dc20a435068d12542b6f3b5
EshginCeferli <eshginij@code.edu.az>
2023-01-05 16:04:36 +0400

    A

---
+++ Fiorello-backend/EntityFrameworkProject/Areas/AdminArea/Controllers/SliderTextController.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderTextController : Controller
+    {
+        private AppDbContext _context;
+        public SliderTextController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            SliderDetail sliderDetail = _context.SliderDetails.Where(m=>!m.IsDeleted).FirstOrDefault();
+            return View(sliderDetail);
+        }
+    }
+}
